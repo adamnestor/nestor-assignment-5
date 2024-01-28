6ddab4066897cf5ca7db8427733d422e14f380e1
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-01-28 16:53:38 -0500

    Create CustomListApplication.java

---
+++ src/test/java/com/coderscampus/CustomListApplication.java
@@ -0,0 +1,15 @@
+package com.coderscampus;
+
+import main.java.com.coderscampus.arraylist.CustomArrayList;
+
+public class CustomListApplication {
+
+	public static void main(String[] args) {
+		
+		CustomArrayList<String> testCustomArrayList = new CustomArrayList<>();
+		
+		
+
+	}
+
+}
